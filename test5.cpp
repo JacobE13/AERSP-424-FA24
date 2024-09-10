43384b517003f962b0b8232a9d1fbda96e7cad11
Jacob Earnest <jae5644@psu.edu>
2024-09-10 12:07:08 -0400

    Update test5.cpp

--- test5.cpp
+++ test5.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+    cout << "Goodbye World!" << endl;
+    int a, b, c, sum;
+    a = 10;
+    b = 20;
+    c= 30;
+    sum = a + b + c;
+    cout << sum << endl;
     return 0;
 }
